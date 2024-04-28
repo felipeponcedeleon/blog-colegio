ac1b2beef1be91b7ee7f60a7ff0b6c1419f61330
felipeponcedeleon <felipeponcedeleon@outlook.com>
2024-04-28 17:03:36 -0400

    Conexión MySQL

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodejs'
+});
+
+db.connect((error) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log('MySQL conectado.')
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('<h1>Home Page</h1>')
